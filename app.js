fdc5401cdb209c36ac31c4f9754c614f9497a981
David Wang <dwang21@stanford.edu>
2014-02-12 11:56:07 +0000

    Time to get started on messing with people's calendars

--- app.js
+++ app.js
@@ -7,10 +7,22 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
+var googleapis = require('googleapis');
+var OAuth2Client = googleapis.OAuth2Client;
+var gcal = require('google-calendar');
 
 var index = require('./routes/index');
 var events = require('./routes/events');
 var settings = require('./routes/settings');
+
+var CLIENT_ID = "93833969413-qi1rveqpc52ut179c40dbdeba5a19k9q.apps.googleusercontent.com";
+var CLIENT_SECRET = "_m1_ZRsGUeo-fb2AMdkltmv8";
+var REDIRECT_URL = "http://localhost:3000/oauth2callback";
+var oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+var calendar_auth_url = oauth2Client.generateAuthUrl({
+  access_type: 'offline',
+  scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar'
+});
 
 // var project = require('./routes/project');
 // Example route
@@ -39,24 +51,70 @@ if ('development' == app.get('env')) {
 }
 
 // Add routes here
-app.get('/', index.view);
-
-// TODO: This needs to get moved to index.js - it needs to save the body data somehow
-app.post('/', function(req, res) {
-	var chunk = '';
-	req.on('data', function(data) {
-      console.log("Received body data:");
-      chunk += data;
-    });
-    req.on('end', function() {
-      console.log("OK " + chunk);
-      res.send({'profile': 'wtf', 'people': 'wtf2'})
-    });
+//app.get('/', index.view);
+app.get('/', function(req, res) {
+	var logged_in = true;
+	if (app.get('tokens') == null)
+		logged_in = false;
+	data = {
+		"calendar_auth_url": calendar_auth_url,
+		"logged_in": logged_in
+	}
+	res.render('index', data);
 });
+
 app.get('/view', events.viewEvents);
 app.get('/create', events.createEvent);
 app.get('/settings', settings.view);
 app.get('/edit', events.editEvent);
+
+var myClient;
+
+  var callback = function(clients) {
+  	myClient = clients;
+  	console.log(clients);
+  }
+
+googleapis
+  .discover('calendar', 'v3')
+  .discover('oauth2', 'v2')
+  .execute(function(err, client){
+    if(!err) {
+      app.set('client', client);
+      // callback(client);
+  	}
+  });
+
+// CAN WE CHANGE THIS TO POST? without getting a redirect_uri_mismatch
+app.get('/oauth2callback', function(req, res) {
+  var code = req.query.code;
+  oauth2Client.getToken(code, function(err, tokens) {
+  	oauth2Client.credentials = tokens;
+  	getData();
+  	app.set('tokens', tokens);
+  	  	
+  	var logged_in = true;
+	if (app.get('tokens') == null)
+		logged_in = false;
+	data = {
+		"calendar_auth_url": calendar_auth_url,
+		"logged_in": logged_in
+	}
+	res.render('index', data);
+  });
+});
+
+// DO SHIT LIKE THIS FOR EVERY GOOGLE API
+var getData = function() {
+	var myClient = app.get('client');
+  myClient.oauth2.userinfo.get().withAuthClient(oauth2Client).execute(function(err, results){
+       console.log(results);
+  });
+  myClient.calendar.calendarList.list().withAuthClient(oauth2Client).execute(function(err, results){
+    // console.log(results);
+  });
+};
+
 // app.get('/project', project.viewProject);
 // app.get('/project/:name', project.viewProject);
 // Example route
