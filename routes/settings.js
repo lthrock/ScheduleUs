59866843bd22915efb4a8fa3db9def846e8f6caa
Elena <elenakfrey@gmail.com>
2014-02-14 01:02:44 +0000

    fixed preferences bug

--- routes/settings.js
+++ routes/settings.js
@@ -5,17 +5,16 @@ exports.view = function(req, res){
 	var startTime = req.query.start;
 	var endTime = req.query.end;
 
-	var currUser;
 	for (var user in users["users"]) {
 		// console.log(users["users"][user]);
 		if (users["users"][user].email == req.session.current_user) {
-			currUser = user;
+			var currUser = user;
 		}
 	}
-	// console.log(currUser);
+	
 	if (startTime)
 		users["users"][currUser].dayStart = startTime;
 	if (endTime)
 		users["users"][currUser].dayEnd = endTime;
-  	res.render('preferences', { 'startTime': users["users"][user].dayStart, 'endTime': users["users"][user].dayEnd });
+  	res.render('preferences', { 'startTime': users["users"][currUser].dayStart, 'endTime': users["users"][currUser].dayEnd });
 };
